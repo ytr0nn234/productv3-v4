2c1aefa10af00285ffbd69d9b59d6235c32c0195
ytr0nn234 <g00467242@atu.ie>
2025-12-16 00:41:29 +0000

    Copied ProductApp.java from v3

--- src/main/java/productv4/ProductApp.java
+++ src/main/java/productv4/ProductApp.java
@@ -5,21 +5,17 @@ import java.util.Scanner;
 public class ProductApp {
 
     public static void main(String[] args) {
-        // display a welcome message
         System.out.println("Welcome to the Product Viewer");
         System.out.println();
 
-        // perform 1 or more selections
         Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
             System.out.print("Enter product code: ");
-            String productCode = sc.nextLine();  // read the product code
+            String productCode = sc.nextLine();
 
-            // Book or a Software Object? = ProductDB.getProduct(productCode);
+            Product p = ProductDB.getProduct(productCode);
 
-            // display the output
-            /*
             System.out.println();
             if (p != null) {
                 System.out.println("Description: " + p.toString());
@@ -31,11 +27,10 @@ public class ProductApp {
             System.out.println();
             System.out.println("Product count: " + Product.getCount() + "\n");
 
-            // see if the user wants to continue
             System.out.print("Continue? (y/n): ");
             choice = sc.nextLine();
             System.out.println();
-            */
         }
+        sc.close();
     }
 }
