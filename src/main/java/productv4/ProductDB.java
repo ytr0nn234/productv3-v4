0e0449ce8e06d2796a0a991409d87f5735d486b9
ytr0nn234 <g00467242@atu.ie>
2025-12-16 00:40:25 +0000

    modified productDB.java

--- src/main/java/productv4/ProductDB.java
+++ src/main/java/productv4/ProductDB.java
@@ -2,11 +2,70 @@ package productv4;
 
 public class ProductDB {
 
-    //  public static Book or Software getProduct(String productCode) {
-    // In a more realistic application, this code would get the data for the product from a file or database
-    // For now, this code just uses if/else statements to return the correct product data
+    public static Product getProduct(String productCode) {
+        Product p = null;
 
-    //We need all the book and software objects, but what kind of object do we return?
-    //}
+        if (productCode.equalsIgnoreCase("java")) {
+            Book b = new Book();
+            b.setCode(productCode);
+            b.setDescription("ATU Java Programming");
+            b.setPrice(57.50);
+            b.setAuthor("Joe Brown");
+            p = b;
+        } else if (productCode.equalsIgnoreCase("jsp")) {
+            Book b = new Book();
+            b.setCode(productCode);
+            b.setDescription("Java Servlets and JSP");
+            b.setPrice(57.50);
+            b.setAuthor("Mike White");
+            p = b;
+        } else if (productCode.equalsIgnoreCase("mysql")) {
+            Book b = new Book();
+            b.setCode(productCode);
+            b.setDescription("Lennon's MySQL");
+            b.setPrice(54.50);
+            b.setAuthor("Jim Lennon");
+            p = b;
+        } else if (productCode.equalsIgnoreCase("studios")) {
+            Software s = new Software();
+            s.setCode(productCode);
+            s.setDescription("Visual Studios");
+            s.setPrice(57.50);
+            s.setVersion("Microsoft 1.1");
+            p = s;
+        } else if (productCode.equalsIgnoreCase("eclipse")) {
+            Software s = new Software();
+            s.setCode(productCode);
+            s.setDescription("Build Java apps");
+            s.setPrice(57.50);
+            s.setVersion("Eclipse Neon");
+            p = s;
+        } else if (productCode.equalsIgnoreCase("oracle")) {
+            Software s = new Software();
+            s.setCode(productCode);
+            s.setDescription("Latest MySQL");
+            s.setPrice(54.50);
+            s.setVersion("Oracle 3.0");
+            p = s;
+        }
+        else if (productCode.equalsIgnoreCase("purple")) {
+            Music m = new Music();
+            m.setCode(productCode);
+            m.setDescription("Fireball");
+            m.setPrice(8.00);
+            m.setArtist("Deep Purple");
+            m.setLabel("Columbia group");
+            p = m;
+        } else if (productCode.equalsIgnoreCase("55pus")) {
+            TV tv = new TV();
+            tv.setCode(productCode);
+            tv.setDescription("Philips 4K UHD LED Smart TV 55PUS7607/12");
+            tv.setPrice(819.00);
+            tv.setManufacture("Philips");
+            tv.setScreenSize("55\"");
+            p = tv;
+        }
+
+        return p;
+    }
 }
-
